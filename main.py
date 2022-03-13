6f9fe4ddcc2fb7ebb74e330692c73ee2d9ae47fa
u5er-name <sashaa0505@gmail.com>
2022-03-13 17:15:25 +0300

    Добавил возможность аутентификации

--- main.py
+++ main.py
@@ -18,6 +18,27 @@ class InstagramBot():
         self.driver.get('https://www.instagram.com')
         time.sleep(5) # пауза для прогрузки главной страницы
 
+    def login(self, username: str, password: str):
+        """Аутентификация
+
+        Args:
+            username (str): имя пользователя
+            password (str): пароль пользователя
+        """
+        print('Вводим имя пользователя...')
+        username_input = self.driver.find_element_by_name("username")
+        username_input.send_keys(username)
+
+        print('Вводим пароль...')
+        password_input = self.driver.find_element_by_name("password")
+        password_input.send_keys(password)  
+        # password_input.send_keys(Keys.ENTER) # симуляция нажатия на enter
+
+        login_button = self.driver.find_element_by_xpath("//button[@class='sqdOP  L3NKy   y3zKF     ']/div")
+        login_button.click()
+        time.sleep(5)
+
 
 driver = webdriver.Firefox()
 bot = InstagramBot(driver)
+bot.login(config.USERNAME, config.PASSWORD)
